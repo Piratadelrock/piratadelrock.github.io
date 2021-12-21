f7868ef6cc2ce80a862c31c2438daa3fac27b3cc
Emmanuel Jose <ejrocko@hotmail.com>
2021-12-20 21:20:07 -0500

    update

--- js/main.js
+++ js/main.js
@@ -12,6 +12,25 @@ $(document).ready(function(){
     });
 });
 
+// otra forma que vi
+// posicionarMenu();
+// $(window).scroll(function () {
+//     posicionarMenu();
+// });
+
+// function posicionarMenu() {
+//     var altura_del_header = $('.header').outerHeight(true);
+//     var altura_del_menu = $('.menu').outerHeight(true);
+
+//     if ($(window).scrollTop() >= altura_del_header) {
+//         $('.menu').addClass('fixed');
+//         $('.wrapper').css('margin-top', (altura_del_menu) + 'px');
+//     } else {
+//         $('.menu').removeClass('fixed');
+//         $('.wrapper').css('margin-top', '0');
+//     }
+// }
+
 // var randomColor = "#"+((1<<24)*Math.random()|0).toString(16); 
 
 // document.documentElement.style.setProperty('main-bg-color', randomColor);
