bf0ec3196db1813fc5cb7776353a7e11f6dcb4f7
Emmanuel Lopera <20448841+Piratadelrock@users.noreply.github.com>
2024-08-07 22:59:20 -0500

    update instagram

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-$(document).ready(function(){
-    // offset espacio usado .top de la altura que hay entre la parte superior y el menu
-	var altura = $('.menu').offset().top;
+$(document).ready(function () {
+  // offset espacio usado .top de la altura que hay entre la parte superior y el menu
+  var altura = $(".menu").offset().top;
 
-    $(window).on('scroll', function(){
-        // scrollTop se da cuenta que sobrepasa la altura y se movera 
-        if ( $(window).scrollTop() > altura) {
-            $('.menu').addClass('menu-fixed');
-        } else {
-            $('.menu').removeClass('menu-fixed');
-        }
-    });
+  $(window).on("scroll", function () {
+    // scrollTop se da cuenta que sobrepasa la altura y se movera
+    if ($(window).scrollTop() > altura) {
+      $(".menu").addClass("menu-fixed");
+    } else {
+      $(".menu").removeClass("menu-fixed");
+    }
+  });
 });
+
+var variable = "hola mundo";
+
+alert(variable);
+
+mock = new Mock("Carlitos");
 
 // otra forma que vi
 // posicionarMenu();
@@ -31,13 +37,9 @@ $(document).ready(function(){
 //     }
 // }
 
-// var randomColor = "#"+((1<<24)*Math.random()|0).toString(16); 
+// var randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
 
 // document.documentElement.style.setProperty('main-bg-color', randomColor);
-
-
-
-
 
 // contador de visitas
 // if (localStorage.pagecount){
